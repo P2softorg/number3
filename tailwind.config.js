fc67211fd285a4a9425ec829bbfd0f5103565049
Philip Tharakan <tharakan22@gmail.com>
2021-09-09 12:44:41 -0400

    added customizations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,48 @@
+// const colors = require('tailwindcss/colors')
+// module.exports = {
+//   purge: [],
+//   darkMode: false, // or 'media' or 'class'
+//    theme: {
+//     colors: {
+//       'test-blue': {
+//         100: '#d0e7fb',
+//         DEFAULT: '#0063bd',
+//         500: '#4299e1',
+//         700: '#2b6cb0',
+       
+//       },
+//       transparent: 'transparent',
+//       current: 'currentColor',
+//       black: colors.black,
+//       white: colors.white,
+//       gray: colors.trueGray,
+//       indigo: colors.indigo,
+//       red: colors.rose,
+//       yellow: colors.amber,
+//       teal: colors.teal
+//     }
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      'test-blue': {
+        100: '#d0e7fb',
+        DEFAULT: '#0063bd',
+        500: '#4299e1',
+        700: '#2b6cb0',
+        
+      },
+      green: colors.green,
+      teal: colors.teal
+    },
   },
-  variants: {
-    extend: {},
-  },
+  variants: {},
   plugins: [],
 }
